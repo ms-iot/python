b0f210eb97921ff8634b5f3badc1181e0a8a86af
Juanya Williams <juanyaw@microsoft.com>
2015-03-30 16:36:41 -0700

    Adding solution file and starting to change to use ComPtr and IInspectable instead of just raw pointers

--- wini2c/i2capi.cpp
+++ wini2c/i2capi.cpp
@@ -8,29 +8,35 @@ using namespace concurrency;
 using namespace Windows::Devices::I2c;
 using namespace Platform;
 using namespace Platform::Collections;
+using namespace Microsoft::WRL;
 
 extern "C" {
 	void* new_i2cdevice(char *deviceId, int address) {
-		I2cDevice^* newDevice = NULL;
+		ComPtr<IInspectable> newDevice = nullptr;
 		I2cConnectionSettings^ settings = ref new I2cConnectionSettings(address);
 		std::string strDeviceId = deviceId;
 		auto wstrDeviceId = std::wstring(strDeviceId.begin(), strDeviceId.end());
 
-		create_task(I2cDevice::FromIdAsync(ref new String(wstrDeviceId.c_str()), settings)).then([&newDevice](I2cDevice^ device) {
-			newDevice = new I2cDevice^();
-			*newDevice = device;
+		create_task(I2cDevice::FromIdAsync(ref new String(wstrDeviceId.c_str()), settings)).then(
+				[&newDevice](I2cDevice^ device) {
+			newDevice = reinterpret_cast<IInspectable*>((Object^)device);
 		}).wait();
 
-		return newDevice;
+		return newDevice.Detach();
     }
 
 	void delete_i2cdevice(void* device) {
 		if (device != NULL) {
-			I2cDevice^* realDevice = (I2cDevice^*)device;
+			ComPtr<IInspectable> realDevice;
+			
+			realDevice.Attach((IInspectable*)device);
+		}
+	}
 
-			*realDevice = nullptr;
-
-			delete realDevice;
+	void write_i2cdevice(void* device, byte data) {
+		if (device != NULL) {
+			ComPtr<IInspectable> realDevice = (IInspectable*)device;
+			I2cDevice^ i2cDevice = (I2cDevice^)reinterpret_cast<Object^>(realDevice.Get());
 		}
 	}
 }
